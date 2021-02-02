42eb08e8912880a94fa9a70d47cd7236acb75443
M Thaduri23 <madhubabu.thaduri@gmail.com>
2021-02-01 20:21:49 -0500

    Add files via upload

---
+++ ICalculator.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public interface ICalculator {
+	
+	public int add(String ...numbers);
+	
+	public int substract(String ...numbers);
+	
+	public int multiply(String ...numbers);
+
+}
